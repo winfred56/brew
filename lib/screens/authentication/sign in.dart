7973e5e3e01400eba9ada713481ae526891c3703
winfred <winfred242002@gmail.com>
2022-07-17 22:42:06 +0000

    :fast_forward: Anonymous Sign In

---
+++ lib/screens/authentication/sign in.dart
@@ -0,0 +1,43 @@
+import 'package:brew/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  // create an instance of the AuthService class to access its methods/functions
+  AuthService _auth = AuthService();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        title: const Text("Sign In To Brew"),
+        elevation: 0.0,
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: ElevatedButton(
+          onPressed: () async {
+            // async because it has to wait for the SignInAnon function to complete
+            dynamic result = _auth.toString();
+            // check if the function run successfully
+            if(result == null){
+              print("Error trying to Sign In");
+            }else{
+              print("Sign In Successful");
+              print(result);
+            }
+          },
+          child: const Text("Sign In Anonymously"),
+        ),
+      ),
+    );
+  }
+}
