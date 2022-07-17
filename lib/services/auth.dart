22b6f5d99c54daedbce7b3a23848fb2973792e88
winfred <winfred242002@gmail.com>
2022-07-17 22:11:01 +0000

    :runner: Anonymous sign in

---
+++ lib/services/auth.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+  // create an instance of the Firebase_auth
+  // _underscore before a variable makes the variable private. Thus you can't use it outside this file
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign In Anonymously
+  Future signInAnon() async {
+    try {
+      UserCredential results = await _auth.signInAnonymously();
+      User? user = results.user;
+      return user;
+
+    } on Exception catch (e) {
+      print(e);
+      return null;
+    }
+
+  }
+
+  // Sign In with Email and Password
+
+  //Register with Email and Password
+
+  // Sign Out
+}
