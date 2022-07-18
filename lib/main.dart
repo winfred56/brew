e22fed9227fa0221ea7abe0f7de9aad69ac9839d
winfred <winfred242002@gmail.com>
2022-07-18 01:46:08 +0000

    :zap: update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:brew/screens/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
