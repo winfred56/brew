2f56c2de402feb974700bd6815762481fdd42487
winfred <winfred242002@gmail.com>
2022-07-19 13:07:42 +0000

    :happy: list builder

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,10 @@
 import 'package:brew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew/services/database.dart';
+import 'package:brew/models/brew.dart';
+
+import 'brew_list.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,29 +13,33 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     // Create an instance of AuthService class
     AuthService _auth = AuthService();
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        title: const Text("Sign In To Brew"),
-        elevation: 0.0,
-        actions: [
-          TextButton.icon(
-              onPressed: ()async{
-                await _auth.signOut();
-              },
-              icon: const Icon(
-                  Icons.logout,
-                color: Colors.white,
-              ),
-              label: const Text(
-                "Logout",
-              style: TextStyle(
-                color: Colors.white
-              ),
-              ))
-          ,
-        ],
+    return StreamProvider<List<Brew>?>.value(
+      value: DatabaseService.withoutUID().brews,
+      initialData: null,
+      child: Scaffold(
+        backgroundColor: Colors.brown[100],
+        appBar: AppBar(
+          backgroundColor: Colors.brown[400],
+          title: const Text("Sign In To Brew"),
+          elevation: 0.0,
+          actions: [
+            TextButton.icon(
+                onPressed: ()async{
+                  await _auth.signOut();
+                },
+                icon: const Icon(
+                    Icons.logout,
+                  color: Colors.white,
+                ),
+                label: const Text(
+                  "Logout",
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                )),
+          ],
+        ),
+        body: const BrewList(),
       ),
     );
   }
