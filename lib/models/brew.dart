86375a5f182edfa2311cdc976da67a8231c31a72
winfred <winfred242002@gmail.com>
2022-07-19 13:09:51 +0000

    :boom: list builder

---
+++ lib/models/brew.dart
@@ -0,0 +1,8 @@
+class Brew{
+  late String sugars;
+  late String name;
+  late int strength;
+
+  Brew({required this.strength,required this.name,required this.sugars});
+
+}
---
+++ lib/screens/home/brew_list.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew/models/brew.dart';
+
+import 'brew_tile.dart';
+
+
+class BrewList extends StatefulWidget {
+  const BrewList({Key? key}) : super(key: key);
+
+  @override
+  State<BrewList> createState() => _BrewListState();
+}
+
+class _BrewListState extends State<BrewList> {
+  @override
+  Widget build(BuildContext context) {
+    final brews = Provider.of<List<Brew>?>(context);
+
+    return ListView.builder(
+      itemCount: brews?.length,
+        itemBuilder: (context, index){
+        return brews!= null ? BrewTile(brew: brews[index]) : Container() ;
+        }
+    );
+  }
+}
